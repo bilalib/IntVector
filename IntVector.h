f306e41e28c3caa01d839b206a714e4ac57c264a
unknown <bilalib@umich.edu>
2020-05-27 13:28:29 -0400

    Changed powers to a pointer to vector. Added vectorize(), get(), and assign()

--- IntVector.h
+++ IntVector.h
@@ -3,8 +3,10 @@
 #include <math.h>
 #include <limits>
 #include <memory>
+#include <vector>
+#include <algorithm>
 
-class SizeTooBigError {};
+struct MaxEltTooBigError {};
 
 template<typename IntType>
 class IntVector
@@ -12,47 +14,72 @@ class IntVector
 private:
 
   IntType container;
-  std::shared_ptr<IntType[]> powers;
+
+  /*
+    The data is stored in base max_elt, where max_elt is given to the constructor.
+    powers stores all the powers of max_elt that fit in IntType, and powers[i] = max_elt^(i+1).
+  */
+  std::shared_ptr<std::vector<IntType>> powers;
 
 public:
 
-  static unsigned capacity(unsigned short size) {
-    return log_with_base(size, std::numeric_limits<IntType>::max());
-  }
 
-  IntVector(unsigned short size_in) 
+  IntVector(unsigned short max_elt_in) 
   {
-    unsigned curr_capacity = capacity(size_in);
-    if (curr_capacity < 2) {
-      throw SizeTooBigError();
+    unsigned short capacity = calc_capacity(max_elt_in);
+    if (capacity < 2) {
+      throw MaxEltTooBigError();
     }
-    powers = std::unique_ptr<IntType[]>(new IntType[curr_capacity]);
-    powers[0] = size_in;
-    for (unsigned short i = 1; i < size_in; ++i) {
-      powers[i] = size_in * powers[i - 1];
+    powers = std::shared_ptr<std::vector<IntType>>(new std::vector<IntType>(capacity));
+    powers->at(0) = max_elt_in;
+    for (unsigned short i = 1; i < max_elt_in; ++i) {
+      powers->at(i) = max_elt_in * powers->at(i - 1);
     }
   }
 
-  IntVector(const std::shared_ptr<IntType[]> const powers_in)
+  IntVector(std::shared_ptr<std::vector<IntType>> powers_in)
     : powers(powers_in)
   {}
 
-  IntVector(const IntVector<IntType>& other)
-    : IntVector(other.powers)
-  {
-    container = other.container;
+  IntVector(const IntVector<IntType>& other) {
+    *this = other;
   }
 
-  const std::shared_ptr<IntType[]> const get_powers() {
-    return powers;
+  IntVector(const std::vector<unsigned short>& input) 
+    : IntVector(*std::max_element(input.begin(), input.end()))
+  {
+    for (unsigned short i = 0; i < input.size(); ++i) {
+      assign(i, input[i]);
+    }
   }
+
+  std::shared_ptr<std::vector<IntType>> get_powers() const { return powers; }
+
+  unsigned short get(unsigned short i) const {
+    return container % powers->at(i);
+  }
+
+  void assign(unsigned short i, unsigned short number) {
+    IntType addition = number * (i == 0 ? 1 : powers->at(i - 1));
+    container = container - get(i) + addition;
+  }
+
+  std::vector<unsigned short> vectorize() {
+    std::vector<unsigned short> data(powers->size());
+    for (unsigned short i = 0; i < data.size(); ++i) {
+      data[i] = get(i);
+    }
+  }
+
 
 private:
 
-  static int log_with_base(double base, double x) {
-    return (int)(log(x) / log(base));
+  static unsigned short log_with_base(double base, double x) {
+    return (unsigned short)(log(x) / log(base));
   }
 
-
+  static unsigned short calc_capacity(unsigned short max_elt) {
+    return log_with_base(max_elt, std::numeric_limits<IntType>::max());
+  }
 };
 
